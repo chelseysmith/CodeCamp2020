7ddc8d8028301ece3adf04e189549bd27e99e037
Chelsey Smith <csmith@wilsonelectronics.com>
2020-11-13 13:21:18 -0700

    Added basic character controller. Setup new input system. Updated Menu scene to use new input and have button selection working with controller.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject _playButton;
+
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(_playButton);
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("Opening");
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] float _playerSpeed;
+    [SerializeField] float _rotationSpeed;
+
+    CharacterController _characterController;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var leftStick = Gamepad.current.leftStick.ReadValue();
+        var rightStick = Gamepad.current.rightStick.ReadValue();
+
+        if(Gamepad.current.buttonSouth.wasPressedThisFrame)
+        {
+            Debug.Log("A");
+        }
+
+        if (Gamepad.current.leftTrigger.wasPressedThisFrame)
+        {
+            Debug.Log($"Left trigger: {Gamepad.current.leftTrigger.ReadValue()}");
+        }
+
+        var direction = new Vector3(leftStick.x, 0, leftStick.y).normalized;
+        if(direction.magnitude >= 0.1f)
+        {
+            var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            _characterController.Move(direction * Time.deltaTime * _playerSpeed);
+        }
+    }
+}
