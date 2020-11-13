9122901bb01a190f4e157a0d9770c5f0179a6c6e
Chelsey Smith <csmith@wilsonelectronics.com>
2020-11-13 10:22:57 -0700

    Added main menu buttons and scene managment. This template will be used for any other UI interactions.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Opening");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
